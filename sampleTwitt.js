6d4705171825936983926f676a7f6c3c4a6fafd3
Hendri Karisma <situkangsayur@gmail.com>
2014-04-26 22:35:12 +0700

    update to file

--- sampleTwitt.js
+++ sampleTwitt.js
@@ -1,13 +1,14 @@
 var twitterAPI = require('node-twitter-api');
+var fs = require('fs');
 
 var twitter = new twitterAPI({
-    consumerKey: 'D5NfrZp4........',
-    consumerSecret: 'YkPdi1giCz3WYmPYB.........zncNiks',
+    consumerKey: 'D5NfrZp4DzELTueZbLspnA',
+    consumerSecret: 'YkPdi1giCz3WYmPYBesR1xMQDG3ks2UgqRK9zncNiks',
     callback: 'https://twitter.com/zack_syah'
 });
 
-var accessToken = '22548834...............hudzufUo',
-    accessTokenSecret = 'wlJ80gWicFx82V...........a7FuPm';
+var accessToken = '2254883460-oXvXGdZvxNU4nYojVeMqO2CjawGvsy0hudzufUo',
+    accessTokenSecret = 'wlJ80gWicFx82VFxCLegYzZJk0qUmxc6rA8rNAua7FuPm';
 var twitt = 'syalalala... nodejs is the best broh...!!!'; 
 var i=0;
 twitter.getTimeline("user",{
@@ -22,10 +23,16 @@ twitter.getTimeline("user",{
 		console.log('error');
         } else {
 		
+		var temp = '';
             // data contains the data sent by twitter
 		for(i=0;i<130;i++){
+			temp = temp + i+' '+data[i]["text"]+'\n';
+			
 			console.log(i+' '+data[i]["text"]+'\n');
 		}
+		fs.writeFile('twitter-result.txt', temp, function(err){
+			if(err) return console.log('error file '+ err);
+		});
 	} 
     }
 );
