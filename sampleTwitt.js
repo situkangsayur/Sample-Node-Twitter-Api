0f8899735a341cbfa483b68a875c764afea75d15
Hendri Karisma <situkangsayur@gmail.com>
2014-04-27 11:47:37 +0700

    update looping

--- sampleTwitt.js
+++ sampleTwitt.js
@@ -2,13 +2,13 @@ var twitterAPI = require('node-twitter-api');
 var fs = require('fs');
 
 var twitter = new twitterAPI({
-    consumerKey: 'D5NfrZp4DzELTueZbLspnA',
-    consumerSecret: 'YkPdi1giCz3WYmPYBesR1xMQDG3ks2UgqRK9zncNiks',
+    consumerKey: 'D5NfrZp4Dz..............bLspnA',
+    consumerSecret: 'YkPdi1giC..........1xMQDG........K9zncNiks',
     callback: 'https://twitter.com/zack_syah'
 });
 
-var accessToken = '2254883460-oXvXGdZvxNU4nYojVeMqO2CjawGvsy0hudzufUo',
-    accessTokenSecret = 'wlJ80gWicFx82VFxCLegYzZJk0qUmxc6rA8rNAua7FuPm';
+var accessToken = '2254883460-o...............................sy0hudzufUo',
+    accessTokenSecret = 'wlJ80gWi..........................a7FuPm';
 var twitt = 'syalalala... nodejs is the best broh...!!!'; 
 var i=0;
 twitter.getTimeline("user",{
